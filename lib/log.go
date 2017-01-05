28cc263db68a30cbdab670e0715869d861ccde93
HANAI tohru <tohru@reedom.com>
2017-01-06 00:02:01 +0900

    Add ErrorLog

--- lib/log.go
+++ lib/log.go
@@ -9,8 +9,12 @@ import (
 // Verbose exports verbose logger.
 var Verbose *log.Logger
 
+// ErrorLog exports error logger.
+var ErrorLog *log.Logger
+
 func init() {
 	Verbose = log.New(ioutil.Discard, "[verbose] ", 0)
+	ErrorLog = log.New(os.Stderr, "Error: ", 0)
 }
 
 // EnableVerboseLog enables verbose log functionality.
