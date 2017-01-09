bab096991a3e08ec6389c758944c898fa1496439
HANAI tohru <tohru@reedom.com>
2017-01-09 17:09:53 +0900

    Add errors

---
+++ lib/errors.go
@@ -0,0 +1,11 @@
+package refcode
+
+import (
+	"github.com/pkg/errors"
+)
+
+// ErrBinaryFile means that the file is likely a binary data file.
+var ErrBinaryFile = errors.New("content is likely binary data")
+
+// ErrNotFound returns when item(file, etc.) is not found.
+var ErrNotFound = errors.New("not found")
